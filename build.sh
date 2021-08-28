4b642f1eb8713a1350ff70820f34a1c4fe2d581f
delivey <deliveydev@gmail.com>
2021-08-28 13:28:49 +0300

    added windows openssl library link

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 if [[ "$OSTYPE" == "win32" ]]; then
-    g++ core.cpp -o core -lssl -lcrypto  -lws2_32 && ./core
+    g++ core.cpp -o core -lssl -lcrypto -lws2_32 -I C:/OpenSSL/include && ./core
 else 
     g++ core.cpp -o core -lssl -lcrypto && ./core
 fi
