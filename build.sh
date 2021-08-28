2aee52f8dfa296b89ae23216a979892a55e583b2
andrei <aosxap@gmail.com>
2021-08-28 11:15:03 +0300

    added win compile command and fixed err

--- build.sh
+++ build.sh
@@ -1,1 +1,5 @@
-g++ core.cpp -o core -lssl -lcrypto && ./core
+if [[ "$OSTYPE" == "win32" ]]; then
+    g++ core.cpp -o core -lssl -lcrypto  -lws2_32 && ./core
+else 
+    g++ core.cpp -o core -lssl -lcrypto && ./core
+fi
--- core.cpp
+++ core.cpp
@@ -14,6 +14,7 @@
     #include <netdb.h>
     #include <unistd.h>
     #include <errno.h>
+    #include <cstring>
 #endif
 
 
