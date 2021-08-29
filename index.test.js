587561c1104d481a15848c501388b51d0a6f1816
andrei <aosxap@gmail.com>
2021-08-29 17:19:01 +0300

    added tests

---
+++ index.test.js
@@ -0,0 +1,45 @@
+const fs = require('fs');
+let file = "./index.html" , dir = "./build";
+
+
+let rm_file = () => {
+    fs.unlink(file , (err) =>{
+        if(err) console.log(err);
+    })    
+}
+
+let rm_dir = () => {
+    if(fs.existsSync(dir)){
+        fs.rmdir(dir, { recursive: true }, (err) => {
+            if (err) {
+                throw err;
+            }
+        });
+    }
+}
+
+if (fs.existsSync(file)) rm_file();
+else if (fs.existsSync(dir)) rm_dir();
+
+
+const {execFileSync} = require("child_process");
+    execFileSync("./build.sh");
+
+const reqless = require('./index');
+
+execFileSync('wget', ['http://info.cern.ch/']);
+
+const read_file = fs.readFileSync('./index.html' , {encoding : 'utf-8'});
+    const response = reqless.req("http://info.cern.ch/", "GET");
+
+test("reqless test" , () => {
+    expect(response.body.trim()).toBe(read_file.trim());
+})
+
+fs.unlink(file , (err) =>{
+    if(err) console.log(err);
+})
+
+fs.rm(dir, { recursive: true }, (err) => {
+    if (err) { throw err;}
+});
