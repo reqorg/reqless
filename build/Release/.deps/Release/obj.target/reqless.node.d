17b06cb94176cd6a74c6dfe2135d4231adad0320
delivey <deliveydev@gmail.com>
2021-08-29 14:39:45 +0300

    Removed build folder

--- build/Release/.deps/Release/obj.target/reqless.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/obj.target/reqless.node := g++ -shared -pthread -rdynamic -m64  -Wl,-soname=reqless.node -o Release/obj.target/reqless.node -Wl,--start-group Release/obj.target/reqless/reqless.o -Wl,--end-group 
--- build/Release/.deps/Release/reqless.node.d
+++
@@ -1,1 +0,0 @@
-cmd_Release/reqless.node := rm -rf "Release/reqless.node" && cp -af "Release/obj.target/reqless.node" "Release/reqless.node"
